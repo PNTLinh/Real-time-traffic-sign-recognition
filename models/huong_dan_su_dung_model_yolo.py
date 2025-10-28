04d76d22ac2222e0aab6d923a40dbe45991e0782
PearlHanh <hanhhiennn15@gmail.com>
2025-10-28 17:10:38 +0700

    update yolo guide

--- models/huong_dan_su_dung_model_yolo.py
+++ models/huong_dan_su_dung_model_yolo.py
@@ -1,13 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO(r"C:\Users\Admin\OneDrive\Desktop\deeplearning_project\Real-time-traffic-sign-recognition\weights\yolo\best.pt")  # Sử dụng model đã được huấn luyện trước đó
+model = YOLO(r"C:\Users\Admin\OneDrive\Desktop\deeplearning_project\Real-time-traffic-sign-recognition\weights\yolo\best.onnx")
 
-results = model.predict(
-    source=r"C:\Users\Admin\OneDrive\Desktop\deeplearning_project\Real-time-traffic-sign-recognition\datasets\processed\processed_test\images\1bc9a8b5e69221cc788336_jpg.rf.b6e3c3f345fb6f264dca0d34aa28a9f6.jpg", # gan source la duong dan den anh hoac video can du doan
-    imgsz=320,       # kích thước resize về cùng với khi train
-    conf=0.25,       # ngưỡng confidence (0.25 là mức ổn)
-    save=True,       # lưu ảnh kết quả có bounding box
-    show=True        # hiện ảnh ra màn hình (có thể tắt nếu chạy server)
+result = model.predict(
+    source= r"C:\Users\Admin\OneDrive\Desktop\deeplearning_project\Real-time-traffic-sign-recognition\data\181829-867371711_small.mp4",
+    imgsz=2650,
+    show=True,
+    save=True
 )
-# Sau do no se in ra ket qua du doan tren anh/video va luu anh/video co bounding box vao thu muc runs/predict
-
